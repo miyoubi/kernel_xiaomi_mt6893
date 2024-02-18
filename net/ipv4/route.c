57c77b4d96ed3842fca015c4d46b9a21c9e6f24e
AbzRaider <99021451+AbzRaider@users.noreply.github.com>
2024-02-18 10:44:13 +0000

    net/ipv4: Fix redefinition of function `rt_add_uncached_list`
    Change-Id: Ide9c46a486f8cd4be388fef0726b9d657b9898f6

--- net/ipv4/route.c
+++ net/ipv4/route.c
@@ -1515,16 +1515,6 @@ struct uncached_list {
 
 static DEFINE_PER_CPU_ALIGNED(struct uncached_list, rt_uncached_list);
 
-void rt_add_uncached_list(struct rtable *rt)
-{
-	struct uncached_list *ul = raw_cpu_ptr(&rt_uncached_list);
-
-	rt->rt_uncached_list = ul;
-
-	spin_lock_bh(&ul->lock);
-	list_add_tail(&rt->rt_uncached, &ul->head);
-	spin_unlock_bh(&ul->lock);
-}
 
 void rt_del_uncached_list(struct rtable *rt)
 {
